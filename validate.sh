bebbe25082d4515788790e2f0b381ddbcda6bc2d
Takashi Iwamoto <hello@iwamot.com>
2025-03-18 22:50:14 +0900

    Enable branch coverage and fix Codecov upload issue

--- validate.sh
+++ validate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 pip install -r requirements.txt
 pip install black && black ./*.py ./app/*.py ./tests/*.py
-pip install pytest pytest-cov && pytest --cov=main --cov=app .
+pip install pytest pytest-cov && pytest --cov=main --cov=app --cov-branch .
 pip install flake8 && flake8 ./*.py ./app/*.py ./tests/*.py
 pip install mypy boto3 types-requests && mypy ./*.py ./app/*.py ./tests/*.py
